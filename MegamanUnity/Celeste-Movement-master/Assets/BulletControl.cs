18a69e4f15d06c6b78dc20606e5ef4c9e8bea5b3
Zero <47561709+Zer077@users.noreply.github.com>
2020-02-26 15:55:58 +0100

    v0.1

---
+++ MegamanUnity/Celeste-Movement-master/Assets/BulletControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+
+    public Rigidbody2D bulletRB;
+    public float bulletSpeed;
+    public float  bulletLife; 
+
+        void Awake() {
+
+        bulletRB = GetComponent<Rigidbody2D>();
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Movement movimiento = player.GetComponent<Movement>();
+        if (movimiento.side == -1)
+        {
+
+            bulletRB.velocity = new Vector2(-bulletSpeed, bulletRB.velocity.y);
+
+
+        }
+        else {
+
+            bulletRB.velocity = new Vector2(bulletSpeed, bulletRB.velocity.y);
+
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        Destroy(gameObject, bulletLife);
+    }
+}
