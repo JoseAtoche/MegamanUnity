4b0c04f350bb058447fb027ae10aafd4d7eacd04
Zero <47561709+Zer077@users.noreply.github.com>
2020-03-09 19:23:38 +0100

    v0.5.7

---
+++ MegamanUnity/Celeste-Movement-master/Assets/BossController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    // Start is called before the first frame update
+      private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int aleatorio = Random.Range(0, 10);
+        switch (aleatorio) { 
+        
+        
+        
+        }
+
+
+
+        anim.SetBool("guillotine", true);
+    }
+}
---
+++ MegamanUnity/Celeste-Movement-master/Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject jugador;
+    private Vector3 posicion;
+
+    
+    
+    void Start()
+    {
+        posicion = transform.position - jugador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jugador.transform.position + posicion;
+
+    }
+}
